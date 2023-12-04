2e518bde7e57f4bea7496543066ff89f06e37c5d
A1945664 <Antoine.Canaguier-Durand@saint-gobain.com>
2023-12-04 17:22:36 +0100

    add data curling

--- tools.py
+++ tools.py
@@ -1,8 +1,23 @@
+import os
+
 def read(day, test=False):
     s = str(day).zfill(2)
     if test:
         s += "_test"
     f = open(f"data/{s}.txt", "r")
+    lines = f.readlines()
+    lines = [l[:-1] for l in lines]
+    return lines
+
+def get_input(day, year=2023):
+    with open(f"data/keys.txt", "r") as f:
+        keys = f.read().splitlines()
+        AOC_COOKIE = keys[0]
+    dday = str(day).zfill(2)
+    s = f'curl --cookie "session={AOC_COOKIE}" https://adventofcode.com/{year}/day/{day}/input > data/{dday}.txt'
+    os.system(s)
+    
+    f = open(f"data/{dday}.txt", "r")
     lines = f.readlines()
     lines = [l[:-1] for l in lines]
     return lines
